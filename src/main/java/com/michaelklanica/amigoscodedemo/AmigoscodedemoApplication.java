df41537a82bcf7115ac36ad9aa5740dd7cf00714
Michael Klanica <michael.david.klanica@gmail.com>
2021-02-01 22:27:48 -0600

    initial commit

---
+++ src/main/java/com/michaelklanica/amigoscodedemo/AmigoscodedemoApplication.java
@@ -0,0 +1,13 @@
+package com.michaelklanica.amigoscodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelklanica/amigoscodedemo/AmigoscodedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelklanica.amigoscodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
