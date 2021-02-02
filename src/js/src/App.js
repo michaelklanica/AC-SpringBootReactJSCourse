837418c1267c9e7ecaaddbf18fad4ed1b60e1f40
Michael Klanica <michael.david.klanica@gmail.com>
2021-02-02 11:18:23 -0600

    imported Antd UI framwork and added a table component

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,19 +1,9 @@
-// import './App.css';
-// import { getAllStudents } from './client';
-
-// function App() {
-//   getAllStudents().then(res => res.json().then(students => {
-//     console.log(students);
-//   }));
-//   return <h1>Hello World Spring Boot and React</h1>
-// }
-
-// export default App;
-
-
 import React, { Component } from 'react';
 import './App.css';
 import { getAllStudents } from './client';
+import {
+  Table
+} from 'antd';
 
 class App extends Component {
   state = {
@@ -40,17 +30,40 @@ class App extends Component {
     const { students } = this.state;
 
     if (students && students.length) {
-      return students.map((student, index) => {
-        return (
-          <div key={index}>
-            <h2>{student.studentId}</h2>
-            <p>{student.firstName}</p>
-            <p>{student.lastName}</p>
-            <p>{student.gender}</p>
-            <p>{student.email}</p>
-          </div>
-        );
-      })
+
+      const columns = [
+        {
+          title: 'Student ID',
+          dataIndex: 'studentId',
+          key: 'studentId'
+        },
+        {
+          title: 'First name',
+          dataIndex: 'firstName',
+          key: 'firstName'
+        },
+        {
+          title: 'Last name',
+          dataIndex: 'lastName',
+          key: 'lastName'
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email'
+        },
+        {
+          title: 'Gender',
+          dataIndex: 'gender',
+          key: 'gender'
+        }
+      ];
+
+      return <Table 
+        dataSource={students} 
+        columns={columns} 
+        rowKey='studentId' />;
+
     }
 
 
