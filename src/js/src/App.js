f57e8617f100e5f3de366ffcfa438b193004134e
Michael Klanica <michael.david.klanica@gmail.com>
2021-02-02 07:12:16 -0600

    added react state

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,11 +1,61 @@
+// import './App.css';
+// import { getAllStudents } from './client';
+
+// function App() {
+//   getAllStudents().then(res => res.json().then(students => {
+//     console.log(students);
+//   }));
+//   return <h1>Hello World Spring Boot and React</h1>
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react';
 import './App.css';
 import { getAllStudents } from './client';
 
-function App() {
-  getAllStudents().then(res => res.json().then(students => {
-    console.log(students);
-  }));
-  return <h1>Hello World Spring Boot and React</h1>
+class App extends Component {
+  state = {
+    students: []
+  }
+
+  componentDidMount() {
+    this.fetchStudents();
+  }
+
+  fetchStudents = () => {
+    getAllStudents()
+      .then(res => res.json()
+      .then(students => {
+        console.log(students);
+      this.setState({
+        students
+      });
+    }));
+  }
+
+  render() {
+
+    const { students } = this.state;
+
+    if (students && students.length) {
+      return students.map((student, index) => {
+        return (
+          <div key={index}>
+            <h2>{student.studentId}</h2>
+            <p>{student.firstName}</p>
+            <p>{student.lastName}</p>
+            <p>{student.gender}</p>
+            <p>{student.email}</p>
+          </div>
+        );
+      })
+    }
+
+
+    return <h1>No Students Found</h1>
+  }
 }
 
 export default App;
